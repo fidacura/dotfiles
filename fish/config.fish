3f2d6e448dd94e9d491f69c03e74b8466c86076c
fidacura <hello@fidacura.eu>
2020-05-03 13:30:37 +0100

    fish/iterm/sublime updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,58 @@
+# Options
+set __fish_git_prompt_show_informative_status
+set __fish_git_prompt_showcolorhints
+set __fish_git_prompt_showupstream "informative"
+
+# Colors
+set green (set_color green)
+set magenta (set_color magenta)
+set normal (set_color normal)
+set red (set_color red)
+set yellow (set_color yellow)
+
+set __fish_git_prompt_color_branch magenta --bold
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+
+# Icons
+set __fish_git_prompt_char_cleanstate ' 👍  '
+set __fish_git_prompt_char_conflictedstate ' ⚠️  '
+set __fish_git_prompt_char_dirtystate ' 💩  '
+set __fish_git_prompt_char_invalidstate ' 🤮  '
+set __fish_git_prompt_char_stagedstate ' 🚥  '
+set __fish_git_prompt_char_stashstate ' 📦  '
+set __fish_git_prompt_char_stateseparator ' | '
+set __fish_git_prompt_char_untrackedfiles ' 🔍  '
+set __fish_git_prompt_char_upstream_ahead ' ☝️  '
+set __fish_git_prompt_char_upstream_behind ' 👇  '
+set __fish_git_prompt_char_upstream_diverged ' 🚧  '
+set __fish_git_prompt_char_upstream_equal ' 💯 ' 
+
+
 function fish_prompt
-    echo
-    echo -n (date "+%T")
-    set_color ffafb3
-    echo -n ' [kasper@MacBookPro' 'on '
-	set_color FF0
-    echo -n (pwd)']'
-    set_color normal
-    echo
-    echo -n '⚡️' '' '=> '
+	echo
+  set last_status $status
+
+  set_color $fish_color_cwd
+  #printf '%s' (prompt_pwd)
+  printf '%s' (pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+  echo
+  echo -n "🦩  "
+  set_color normal
+end
+
+function fish_greeting
+	echo
+    fortune | cowsay
+  echo
 end
 
 function tmux2
@@ -16,7 +61,10 @@ function tmux2
 end
 
 # respect node PATH
-set -gx PATH $PATH /Users/kasper/npm-global/bin
+set -gx PATH $PATH /Users/filipe/npm-global/bin
+set PATH /usr/local/Cellar/node/12.8.1/lib/node_modules/@vue/cli/bin/vue.js $PATH 
 
 # grep colors
-setenv -x GREP_OPTIONS "--color=auto"
+# setenv -x GREP_OPTIONS "--color=auto"
+
+
