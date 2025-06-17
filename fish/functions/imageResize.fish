5c88ff17613324555652b4fa6eb4660391e28bc4
fidacura <hello@fidacura.xyz>
2025-06-17 16:39:35 +0100

    chore(dotfiles): remove old configs

--- fish/functions/imageResize.fish
+++
@@ -1,3 +0,0 @@
-function imageResize
-   mogrify -path $argv[3] -filter Triangle -define filter:support=2 -thumbnail $argv[2] -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB $argv[1]
-end
